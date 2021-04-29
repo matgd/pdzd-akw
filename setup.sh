206e73ef5b3056426804334ae998378d71a940b1
matgd <matgd@users.noreply.github.com>
2021-04-29 21:28:12 +0200

    setup update

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 wget https://raw.githubusercontent.com/matgd/pdzd-akw/master/fetch_kaggle_datasets.sh
 wget https://raw.githubusercontent.com/matgd/pdzd-akw/master/cronfile
 
+sudo chmod +x fetch_kaggle_datasets.sh
+sudo chmod +xr cronfile
+
 sudo mkdir /var/ufc/work
 sudo chown cloudera:cloudera /var/ufc/work
 
