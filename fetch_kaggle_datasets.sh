f3b482081418033927c62ab343802c15afadf346
matgd <matgd@users.noreply.github.com>
2021-05-01 19:28:30 +0200

    Update fetch_kaggle_datasets.sh

--- fetch_kaggle_datasets.sh
+++ fetch_kaggle_datasets.sh
@@ -21,8 +21,8 @@ TARGET_DIR_2="${2}"
 DATASET_CSV_1="ufcdata.csv"
 DATASET_CSV_2="ufc-fight-dataset.csv"
 
-echo "[$(date)] ################### STARTING SCRIPT ###################" | tee "${LOGFILE}"
-echo "[$(date)] INFO: Trying to fetch data from kaggle..." | tee "${LOGFILE}"
+echo "[$(date)] ################### STARTING SCRIPT ###################" | tee -a "${LOGFILE}"
+echo "[$(date)] INFO: Trying to fetch data from kaggle..." | tee -a "${LOGFILE}"
 
 function downloadSet() {
     # Parameters:
@@ -30,20 +30,20 @@ function downloadSet() {
     #   $2 - target directory to download
 
     for i in $(seq ${RETRIES}); do
-        echo "[$(date)] INFO: Attempt ${i}/${RETRIES} of fetching dataset ${1}..." | tee "${LOGFILE}"
+        echo "[$(date)] INFO: Attempt ${i}/${RETRIES} of fetching dataset ${1}..." | tee -a "${LOGFILE}"
         printf "[$(date)] KAGGLE: " >> ${LOGFILE}
-        /home/cloudera/.local/bin/kaggle datasets download "${1}" -p "${2}" | tee "${LOGFILE}"
+        /home/cloudera/.local/bin/kaggle datasets download "${1}" -p "${2}" | tee -a "${LOGFILE}"
         if [[ ${PIPESTATUS[0]} -eq 0 ]]; then
             return 0
         fi
-        echo "[$(date)] INFO: Attempt ${i} failed, retry in ${SLEEP_TIME} seconds..." | tee "${LOGFILE}"
+        echo "[$(date)] INFO: Attempt ${i} failed, retry in ${SLEEP_TIME} seconds..." | tee -a "${LOGFILE}"
         sleep ${SLEEP_TIME}
     done
-    echo "[$(date)] ERROR: Failed fetching dataset ${1}" | tee "${LOGFILE}"
+    echo "[$(date)] ERROR: Failed fetching dataset ${1}" | tee -a "${LOGFILE}"
     return 1
 }
 
-echo "[$(date)] INFO: Finished download." | tee "${LOGFILE}"
+echo "[$(date)] INFO: Finished download." | tee -a "${LOGFILE}"
 
 downloadSet rajeevw/ufcdata "${TARGET_DIR_1}"
 FIRST_RESULT=$?
@@ -52,7 +52,7 @@ SECOND_RESULT=$?
 
 EXIT_CODE=$((${FIRST_RESULT} + ${SECOND_RESULT} * 2))
 if [[ "${EXIT_CODE}" -ne 0 ]]; then
-    echo "[$(date)] ERROR: Failed fetching at least one dataset (code: ${EXIT_CODE})" | tee "${LOGFILE}"
+    echo "[$(date)] ERROR: Failed fetching at least one dataset (code: ${EXIT_CODE})" | tee -a "${LOGFILE}"
     exit "${EXIT_CODE}"
 fi
 
@@ -78,29 +78,29 @@ HEAD_2=$(head -n 1 "${TARGET_DIR_2}/${DATASET_CSV_2}")
 HEAD_2_OLD=$(head -n 1 "${TARGET_DIR_2}/${DATASET_CSV_2}.old")
 
 if [[ "${HEAD_1}" != "${HEAD_1_OLD}" ]]; then 
-    echo "[$(date)] ERROR: Mismatched headers ${DATASET_CSV_1} - ${DATASET_CSV_1}.old" | tee "${LOGFILE}"
-    echo "[$(date)] ################### ENDING SCRIPT ###################" | tee "${LOGFILE}"
+    echo "[$(date)] ERROR: Mismatched headers ${DATASET_CSV_1} - ${DATASET_CSV_1}.old" | tee -a "${LOGFILE}"
+    echo "[$(date)] ################### ENDING SCRIPT ###################" | tee -a "${LOGFILE}"
     exit 4
 fi
 if [[ "${HEAD_2}" != "${HEAD_2_OLD}" ]]; then 
-    echo "[$(date)] ERROR: Mismatched headers ${DATASET_CSV_2} - ${DATASET_CSV_2}.old" | tee "${LOGFILE}"
-    echo "[$(date)] ################### ENDING SCRIPT ###################" | tee "${LOGFILE}"
+    echo "[$(date)] ERROR: Mismatched headers ${DATASET_CSV_2} - ${DATASET_CSV_2}.old" | tee -a "${LOGFILE}"
+    echo "[$(date)] ################### ENDING SCRIPT ###################" | tee -a "${LOGFILE}"
     exit 5
 fi
 ### GET DIFFERENCE TO NEW FILE ###
 
 cd "${TARGET_DIR_1}"
 if [[ -f ${DATASET_CSV_1}.old ]]; then
-    echo "[$(date)] INFO: Getting diff of ${DATASET_CSV_1} and ${DATASET_CSV_1}.old" | tee "${LOGFILE}"
+    echo "[$(date)] INFO: Getting diff of ${DATASET_CSV_1} and ${DATASET_CSV_1}.old" | tee -a "${LOGFILE}"
     echo ${HEAD_1} > "${DATASET_CSV_1}".diff
     grep -v -F -f <(sed 's/^[*[:space:]]*//' "${DATASET_CSV_1}") "${DATASET_CSV_1}".old >> "${DATASET_CSV_1}".diff
     if [[ $(wc -l < ${DATASET_CSV_1}.diff) != "1" ]]; then
         mv ${DATASET_CSV_1}.diff /var/ufc/sources/rajeevw_ufcdata/data-$(date "+%Y-%m-%d").csv
     else
-        echo "[$(date)] INFO: No changes in ${DATASET_CSV_1}" | tee "${LOGFILE}"
+        echo "[$(date)] INFO: No changes in ${DATASET_CSV_1}" | tee -a "${LOGFILE}"
     fi
 else
-    echo "[$(date)] INFO: Old files doesn't exist." | tee "${LOGFILE}"
+    echo "[$(date)] INFO: Old files doesn't exist." | tee -a "${LOGFILE}"
     cp "${DATASET_CSV_1}" "${DATASET_CSV_1}".diff
     mv ${DATASET_CSV_1}.diff /var/ufc/sources/rajeevw_ufcdata/data-$(date "+%Y-%m-%d").csv
 fi
@@ -108,13 +108,13 @@ cd -
 
 cd "${TARGET_DIR_2}"
 if [[ -f ${DATASET_CSV_2}.old ]]; then
-    echo "[$(date)] INFO: Getting diff of ${DATASET_CSV_2} and ${DATASET_CSV_2}.old" | tee "${LOGFILE}"
+    echo "[$(date)] INFO: Getting diff of ${DATASET_CSV_2} and ${DATASET_CSV_2}.old" | tee -a "${LOGFILE}"
     echo ${HEAD_2} > "${DATASET_CSV_2}".diff
     grep -v -F -f <(sed 's/^[*[:space:]]*//' "${DATASET_CSV_2}") "${DATASET_CSV_2}".old >> "${DATASET_CSV_2}".diff
     if [[ $(wc -l < ${DATASET_CSV_2}.diff) != "1" ]]; then
         mv ${DATASET_CSV_2}.diff /var/ufc/sources/theman90210_ufc-fight-dataset/data-$(date "+%Y-%m-%d").csv
     else
-        echo "[$(date)] INFO: No changes in ${DATASET_CSV_2}" | tee "${LOGFILE}"
+        echo "[$(date)] INFO: No changes in ${DATASET_CSV_2}" | tee -a "${LOGFILE}"
     fi
 
 else
@@ -129,15 +129,15 @@ cd -
 
 ### MARKING FILES AS 'OLD' ONES
 
-echo "[$(date)] INFO: Marking files as old ones in ${TARGET_DIR_1}..." | tee "${LOGFILE}"
+echo "[$(date)] INFO: Marking files as old ones in ${TARGET_DIR_1}..." | tee -a "${LOGFILE}"
 for f in "${TARGET_DIR_1}/"*.csv; do
     cp "${f}" "${f}.old"
 done
 
-echo "[$(date)] INFO: Marking files as old ones in ${TARGET_DIR_2}..." | tee "${LOGFILE}"
+echo "[$(date)] INFO: Marking files as old ones in ${TARGET_DIR_2}..." | tee -a "${LOGFILE}"
 for f in "${TARGET_DIR_2}/"*.csv; do
     cp "${f}" "${f}.old"
 done
 
-echo "[$(date)] ################### ENDING SCRIPT ###################" | tee "${LOGFILE}"
+echo "[$(date)] ################### ENDING SCRIPT ###################" | tee -a "${LOGFILE}"
 exit 0
