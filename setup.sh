378c000bee213e3935ab94db4b6a4187415eecb8
matgd <matgd@users.noreply.github.com>
2021-05-01 16:55:04 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,11 @@ sudo chmod +xr cronfile
 sudo mkdir /var/ufc/work
 sudo chown cloudera:cloudera /var/ufc/work
 
+sudo mkdir -p /var/ufc/work/sources/rajeevw_ufcdata
+sudo chown cloudera:cloudera /var/ufc/work/sources/rajeevw_ufcdata
+sudo mkdir -p /var/ufc/work/sources/theman90210_ufc-fight-dataset
+sudo chown cloudera:cloudera /var/ufc/work/sources/theman90210_ufc-fight-dataset
+
 sudo mkdir /var/log/ufc
 sudo chown cloudera:cloudera /var/log/ufc
 
